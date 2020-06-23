ef5ddd11cc974969674421a70fbbbd7522e79f42
Sonali Loomba <ohsonali@berkeley.edu>
2020-06-23 15:40:24 -0700

    workingTable

--- XForceCode/src/sample/Controller.java
+++ XForceCode/src/sample/Controller.java
@@ -16,31 +16,41 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML TableView<PartInfo> tableView;
-    @FXML TableColumn<PartInfo, String> figureColumn;
+    /*@FXML TableColumn<PartInfo, String> figureColumn;
     @FXML TableColumn<PartInfo, String> indexColumn;
     @FXML TableColumn<PartInfo, String> cageColumn;
     @FXML TableColumn<PartInfo, String> descriptionColumn;
     @FXML TableColumn<PartInfo, String> unitsColumn;
     @FXML TableColumn<PartInfo, String> usableColumn;
-    @FXML TableColumn<PartInfo, String> SMRColumn;
+    @FXML TableColumn<PartInfo, String> SMRColumn;*/
 
 
     @Override
     public void initialize (URL url, ResourceBundle rb){
+
+        TableColumn figureColumn = new TableColumn("Figure");
+        TableColumn indexColumn = new TableColumn("Index");
+        TableColumn cageColumn = new TableColumn("Cage");
+        TableColumn descriptionColumn = new TableColumn("Description");
+        TableColumn unitsColumn = new TableColumn("Units");
+        TableColumn usableColumn = new TableColumn("Usable");
+        //TableColumn SMRColumn = new TableColumn("smr");
+        tableView.getColumns().addAll(figureColumn, indexColumn, cageColumn, descriptionColumn, unitsColumn, usableColumn);
+
+
         figureColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("figure"));
         indexColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("index"));
         cageColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("cage"));
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("description"));
         unitsColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("units"));
         usableColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("usable"));
-        SMRColumn.setCellValueFactory(new PropertyValueFactory<PartInfo, String>("smr"));
 
         tableView.setItems(getPartInfo());
     }
 
     public ObservableList<PartInfo> getPartInfo(){
         ObservableList<PartInfo> parts = FXCollections.observableArrayList();
-        parts.add(new PartInfo("12", "13", "1234", "lamp", "1", "A", "123"));
+        parts.add(new PartInfo("12", "13", "1234", "lamp", "1", "A"));
         return parts;
     }
 }
